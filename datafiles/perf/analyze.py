782738b5b3df019c20568c2d7c444a9ffcabf4e2
Waqar Ali <Waqar@Macbook.local>
2018-10-08 14:51:11 -0500

    gsched: Modified plotting scropt
    Signed-off-by: Waqar Ali <Waqar@Macbook.local>

--- datafiles/perf/analyze.py
+++ datafiles/perf/analyze.py
@@ -15,7 +15,10 @@ def analyze (filename):
                 time = float (match.group (1))
                 inferTime.append (time)
 
-    return ((sorted (inferTime)) [:-1])
+    sortedTimes = sorted (inferTime)
+    cutoff = np.percentile (sortedTimes, 98)
+    filteredTimes = [t for t in sortedTimes if t < cutoff]
+    return (filteredTimes)
 
 def plotData (data, platform, nn):
     num_bins = 1000
@@ -27,20 +30,23 @@ def plotData (data, platform, nn):
     xLimLeft = min ([min (x) for x in data.values ()])
     xLimRight = max ([max (x) for x in data.values ()])
     xRange = xLimRight - xLimLeft
-    xLimLeft = xLimLeft - margins * xRange
-    xLimRight = xLimRight + margins * xRange
+    xLimLeft = 0 if platform == 'pi' else 5#xLimLeft - margins * xRange
+    xLimRight = 850 if platform == 'pi' else 40#xLimRight + margins * xRange
 
     for exp in data:
         counts, bin_edges = np.histogram (data [exp], bins = num_bins, normed = True)
         cdf = np.cumsum (counts)
         plt.plot (bin_edges [1:], cdf/cdf[-1], lineType [exp], lw = 2.5, label = lineLabels [exp])
-    plt.plot ([xLimLeft, xLimRight], [0.95, 0.95], 'k-', lw = 1)
+    plt.plot ([xLimLeft, xLimRight], [0.95, 0.95], 'k--', lw = 1)
     plt.xlim (xLimLeft, xLimRight)
+    plt.ylim (0, 1.15)
+    plt.xticks (fontsize = 'large', fontweight = 'bold')
+    plt.yticks (fontsize = 'large', fontweight = 'bold')
     plt.ylabel ('CDF', fontsize = fontSizeLabels, fontweight = fontWeightLabels)
     plt.xlabel ('DNN Inference Time (msec)', fontsize = fontSizeLabels, fontweight = fontWeightLabels)
-    plt.legend (loc = 'center right')
+    plt.legend (loc = 'upper center', ncol = 3, fontsize = 'large')
     plt.grid ()
-    plt.savefig ('%s_nn%s.png' % (platform, nn))
+    plt.savefig ('%s_nn%s.pdf' % (platform, nn), bbox_inches = 'tight')
 
     return
 
@@ -48,7 +54,7 @@ def main ():
     data = {}
     platform = sys.argv [1]
     nn = sys.argv [2]
-    cc = 4 - int (nn)
+    cc = 3
     data ['ltimes'] = analyze ('%s/nn%s_%s%d' % (platform, nn, 'locked.corun', cc))
     data ['utimes'] = analyze ('%s/nn%s_%s%d' % (platform, nn, 'unlocked.corun', cc))
     data ['stimes'] = analyze ('%s/nn%s_%s' % (platform, nn, 'unlocked.solo'))
