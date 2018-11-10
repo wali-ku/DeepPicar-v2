9bd25d547282d1d7834f85bf928882ddf10f2945
Waqar Ali <waqar.272@gmail.com>
2018-11-10 09:45:25 +0000

    xavier: Results with bandwidth interference experiments
    Signed-off-by: Waqar Ali <waqar.272@gmail.com>

---
+++ xavier/analyze.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import sys, re
+import matplotlib.pyplot as plt
+
+meanRex = re.compile (r'^mean: ([\d\.]+).*$')
+def parse (filename):
+    meanTime = 0
+    with open (filename, 'r') as fdi:
+        for line in fdi:
+            m = meanRex.match (line)
+
+            if m:
+                meanTime = round (float (m.group (1)), 3)
+                break
+
+    if not meanTime:
+        print '[ERROR] Processing time "0" in file: ', filename
+        sys.exit (-1)
+
+    return meanTime
+
+def plotData (data):
+    fig = plt.figure (figsize = (20, 10))
+
+    # Absolute performance plot
+    p = plt.subplot (2, 1, 1)
+    xticks = sorted ([tick for key in data.keys () for tick in data [key]['ticks']])
+    xtickNames = [tick for key in data.keys () for tick in range (8)]
+
+    for key in data.keys ():
+        plt.bar (data [key]['ticks'], data [key]['perf'], color = 'white',
+                edgecolor = 'black', width = 1, lw = 2, hatch = data
+                [key]['hatch'], label = data [key]['name'])
+
+    plt.gca ().yaxis.grid (True, linestyle = '--')
+    plt.ylabel ('Average Processing Time (msec)', fontweight = 'bold', fontsize = 'large')
+    plt.xticks ([])
+    plt.legend (ncol = 4, loc = 'upper center', fontsize = 'large')
+    plt.ylim (0, 20)
+
+    # Normalized performance plot
+    p = plt.subplot (2, 1, 2)
+    xticks = sorted ([tick for key in data.keys () for tick in data [key]['ticks']])
+    xtickNames = [tick for key in data.keys () for tick in range (8)]
+
+    for key in data.keys ():
+        plt.bar (data [key]['ticks'], data [key]['norm'], color = 'white',
+                edgecolor = 'black', width = 1, lw = 2, hatch = data
+                [key]['hatch'], label = data [key]['name'])
+
+    plt.gca ().yaxis.grid (True, linestyle = '--')
+    plt.ylabel ('Normalized DNN Performance', fontweight = 'bold', fontsize = 'large')
+    plt.xlabel ('Number of Bandwidth Corunners', fontweight = 'bold', fontsize = 'large')
+    plt.suptitle ('Memory Interference Study\nJetson-Xavier', fontsize = 'xx-large', fontweight = 'bold')
+    plt.xticks (xticks, xtickNames)
+    plt.ylim (0, 3)
+    fig.savefig ('xavier_bw.pdf', bbox_inches = 'tight')
+
+    return
+
+def main ():
+    data = {'CR': {'perf': [], 'norm': [], 'name': 'CPU (Read)',  'ticks': range (0, 8),    'hatch': 'xx'},
+            'CW': {'perf': [], 'norm': [], 'name': 'CPU (Write)', 'ticks': range (10, 18),  'hatch': '**'},
+            'GR': {'perf': [], 'norm': [], 'name': 'GPU (Read)',  'ticks': range (20, 28),  'hatch': '--'},
+            'GW': {'perf': [], 'norm': [], 'name': 'GPU (Write)', 'ticks': range (30, 38),  'hatch': '..'}}
+
+    data ['CR']['files'] = ['CPU_bw%d_read.perf'    % x for x in range (8)]
+    data ['CW']['files'] = ['CPU_bw%d_write.perf'   % x for x in range (8)]
+    data ['GR']['files'] = ['GPU_bw%d_read.perf'    % x for x in range (8)]
+    data ['GW']['files'] = ['GPU_bw%d_write.perf'   % x for x in range (8)]
+
+    for scenario in data.keys ():
+        for case in data [scenario]['files']:
+            perf = parse (case)
+            data [scenario]['perf'].append (perf)
+        data [scenario]['norm'] = [x / data [scenario]['perf'][0] for x in data [scenario]['perf']]
+
+    plotData (data)
+
+    return
+
+if __name__ == '__main__':
+    main ()
---
+++ xavier/exp2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+corunners=7
+accessType=('read' 'write')
+execType=('CPU' 'GPU')
+
+for execution in ${execType[@]}; do
+	for access in ${accessType[@]}; do
+		for num_of_corunners in `seq 1 ${corunners}`; do
+			for core in `seq 1 ${num_of_corunners}`; do
+				# Start corunners
+				printf "[STATUS] Starting bandwidth (%s) on Core-%d\n" ${access} ${core}
+				bandwidth -c ${core} -m 16384 -t 10000 -a ${access} &> /dev/null &
+			done
+
+			sleep 2
+			printf "[STATUS] Running DNN on %s with %d corunners\n" ${execution} ${num_of_corunners}
+			if [ "${execution}" == "CPU" ]; then
+				CUDA_VISIBLE_DEVICES='' taskset -c 0 python test-model.py 1 0 2>&1 | tee ${execution}_bw${num_of_corunners}_${access}.perf
+			else
+				taskset -c 0 python test-model.py 1 0 2>&1 | tee ${execution}_bw${num_of_corunners}_${access}.perf
+			fi
+			echo '-------------------------------------------------'
+			killall bandwidth
+		done
+		echo
+	done
+	echo
+done
